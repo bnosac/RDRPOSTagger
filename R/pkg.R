4444d7efedb5e4b726f66e433cc2c43237044004
Jan Wijffels <jwijffels@bnosac.be>
2017-08-03 19:10:34 +0200

    cran compliance

--- R/pkg.R
+++ R/pkg.R
@@ -1,15 +1,7 @@
-#' @title Parts of Speech Tagging based on the Ripple Down Rules-based Part-Of-Speech Tagger
-#' @description Parts of Speech tagging and morphological tagging based on the
-#' Ripple Down Rules-based Part-Of-Speech Tagger (RDRPOS) available at https://github.com/datquocnguyen/RDRPOSTagger.
-#' Tagging models include Bulgarian, Czech, Dutch, English, French, German, Hindi, Italian, Portuguese, Spanish, Swedish, Thai and Vietnamese.
-#' RDRPOSTagger also supports the pre-trained Universal POS tagging models for 40 languages.
-#' @docType package
-#' @name RDRPOSTagger
 #' @import rJava
 #' @importFrom tools file_path_sans_ext
 #' @importFrom utils unzip
 #' @importFrom data.table data.table rbindlist setDT setDF setcolorder setnames
-#' @seealso \code{\link{rdr_pos}}, \code{\link{rdr_model}}
 NULL
 
 
